76c9c5463da44254f5e089ca345e2f82163dc5a8
Blakefive <fivefive050500@gmail.com>
2024-09-03 23:18:50 +0900

    15

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("check");
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        if : id.value,
+        password : password.value,
+    };
+    console.log(req);
+}
